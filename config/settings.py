0ad5ce5af066e8eeb3d71b3d42b240a9c2403acd
App Generator <support@appseed.us>
2021-06-16 11:04:12 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gw&7sjw7afb9bt=5nbd30(lc3l72+nij@-#t)h1x2#ly6ijo6q'
+SECRET_KEY = '<STRONG_KEY_HERE>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
